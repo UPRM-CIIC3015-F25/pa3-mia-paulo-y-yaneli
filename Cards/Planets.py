00f4b19e32e22cec8f8544c8c56707070bf6155a
yaneli <aponteyaneli@gmail.com>
2025-11-19 00:01:31 -0400

    TASK 6.1: PLANET CARD!

--- Cards/Planets.py
+++ Cards/Planets.py
@@ -29,5 +29,69 @@ class PlanetCard:
 #   These cards will be used in the shop and gameplay systems to upgrade specific poker hands.
 
 PLANETS = {
+    "Mercury": PlanetCard(
+        name="Mercury",
+        description="levels up High Card",
+        price=2,
+        chips=10,
+        mult=1
+    ),
+    "Venus": PlanetCard(
+        name="Venus",
+        description="levels up One Pair",
+        price=2,
+        chips=15,
+        mult=1
+    ),
+    "Earth": PlanetCard(
+        name="Earth",
+        description="levels up Two Pair",
+        price=2,
+        chips=15,
+        mult=2
+    ),
+    "Mars": PlanetCard(
+        name="Mars",
+        description="levels up Three of a Kind",
+        price=2,
+        chips=25,
+        mult=2
+    ),
+    "Jupiter": PlanetCard(
+        name="Jupiter",
+        description="levels up Straight",
+        price=3,
+        chips=25,
+        mult=3
+    ),
+    "Saturn": PlanetCard(
+        name="Saturn",
+        description="levels up Flush",
+        price=3,
+        chips=30,
+        mult=3
+    ),
+    "Uranus": PlanetCard(
+        name="Uranus",
+        description="levels up Full House",
+        price=3,
+        chips=35,
+        mult=3
+    ),
+    "Neptune": PlanetCard(
+        name="Neptune",
+        description="levels up Four of a Kind",
+        price=3,
+        chips=40,
+        mult=4
+    ),
+    "Sun": PlanetCard(
+        name="Sun",
+        description="levels up all hands",
+        price=12,
+        chips=30,
+        mult=2
+    )
+
 
 }
